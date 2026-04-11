d524891199c41f6c86aa03af9f1ceabb34e4e4c2
rudolfmuller <rudolfmullercz@gmail.com>
2026-04-11 11:39:08 +0200

    added 'read_license()', added new variant 'DirectoryError' in 'TransactionError', added 'LICENSE_SPACE', 'LICENSE_SUFFIX'

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,14 +3,24 @@ use std::fs;
 use std::io;
 use thiserror::Error;
 
+const LICENSE_SPACE: &str = "license-space";
+const LICENSE_SUFFIX: &str = ".lic";
+
 #[derive(Error, Debug)]
 pub enum TransactionError {
     #[error("failed to read license file")]
     ReadError(#[from] io::Error),
+    #[error("failed to open directory")]
+    DirectoryError,
 }
 
 pub fn read_license(kind: &str) -> Result<String, TransactionError> {
-    let license_path = format!("{}/sources/{}.lic", env!("CARGO_MANIFEST_DIR"), kind);
-    let license_contents: String = fs::read_to_string(license_path)?;
+    let share_dir = dirs::data_dir().ok_or(TransactionError::DirectoryError)?;
+    let license_path = share_dir
+        .join(LICENSE_SPACE)
+        .join(format!("{}{}", kind, LICENSE_SUFFIX));
+    dbg!(&license_path);
+    let license_contents: String = fs::read_to_string(&license_path)?;
+
     Ok(license_contents)
 }
