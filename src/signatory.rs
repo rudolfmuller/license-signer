34dc8b4e7cb60b4d2779ef3ff8dcc6e9a53874a5
rudolfmuller <rudolfmullercz@gmail.com>
2026-04-11 23:21:07 +0200

    added new dependencie, more readable logic from 'sign()' function, and added 'else value'

--- src/signatory.rs
+++ src/signatory.rs
@@ -1,3 +1,5 @@
+use chrono::{Datelike, Local};
+
 pub struct LicenseFields<'a> {
     pub year: Option<i32>,
     pub fullname: Option<&'a str>,
@@ -7,22 +9,12 @@ pub struct LicenseFields<'a> {
 }
 
 pub fn sign(mut paper: String, lf: LicenseFields) -> String {
-    if let Some(val) = lf.year {
-        paper = paper.replace("[year]", &val.to_string());
-    }
-    if let Some(val) = lf.fullname {
-        paper = paper.replace("[fullname]", val);
-    }
-    if let Some(val) = lf.title {
-        paper = paper.replace("[title]", val);
-    }
-    if let Some(val) = lf.contacts {
-        paper = paper.replace("[contacts]", &val.join(", "));
-    }
-    if let Some(val) = lf.program {
-        paper = paper.replace("[program]", val);
-    }
-
+    // WARN: THIS IS WRONG; IT ALLOCATES STRINGS UNNECESSARILY, maybe I'll check it out next time
+    let now = Local::now().year();
+    paper = paper.replace("[year]", &lf.year.unwrap_or(now).to_string());
+    paper = paper.replace("[fullname]", lf.fullname.unwrap_or("[OWNER!]"));
+    paper = paper.replace("[title]", lf.title.unwrap_or("[TITLE!]"));
+    paper = paper.replace("[contacts]", &lf.contacts.unwrap_or_default().join(", "));
+    paper = paper.replace("[program]", lf.program.unwrap_or("[PROGRAM!]"));
     paper
 }
-
